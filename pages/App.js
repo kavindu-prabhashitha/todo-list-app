d08005dc7a417a11773b077990b6e1a4e7d06eba
kavindu <kavindup@99x.io>
2023-07-21 18:28:03 +0530

    project initialized

---
+++ pages/App.js
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+export default function App() {
+  return (
+    <>
+      <h1>My ToDos</h1>
+      <Todo title="Learn React"/>
+      <Todo title="Master React"/>
+      <Todo title="Complete React Course"/>
+    </>
+  );
+}
